1dcd57cfbb30fe68e41bab53cb40c1a10c4d164c
harryOsporn <tinvatit4869@gmail.com>
2024-08-13 14:22:38 +0700

    make cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -82,7 +82,13 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
       });
     }
 
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
     
-    console.log(cart);
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+    
   });
 });
+

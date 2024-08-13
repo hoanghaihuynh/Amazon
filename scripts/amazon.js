2af271a4e290d0d4c44a2e00fa63e1ad7938ed46
harryOsporn <tinvatit4869@gmail.com>
2024-08-13 16:37:43 +0700

    make added visible and insert setTimeout for add-to-cart button

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -42,7 +42,7 @@ products.forEach((product) => {
 
         <div class="product-spacer"></div>
 
-        <div class="added-to-cart">
+        <div class="added-to-cart data-product-id-${product.id}">
           <img src="images/icons/checkmark.png">
           Added
         </div>
@@ -57,12 +57,15 @@ products.forEach((product) => {
 
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
-
+let timeoutId;
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
     // button.dataset sẽ trả về 1 object có dữ liệu của product
     const productName = button.dataset.productName;
     const productId = button.dataset.productId;
+    /*
+      shortcut : const { productId } = button.dataset;
+    */
 
     let matchingItem;
     //Quantity Selector
@@ -84,6 +87,14 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
       });
     }
 
+    //Added 2 giây (1)
+    let added = document.querySelector(`.data-product-id-${productId}`).classList.add('added');
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      document.querySelector(`.data-product-id-${productId}`).classList.remove('added');
+    } ,2000);
+
+    //Số lượng item trong giỏ hàng
     let cartQuantity = 0;
     cart.forEach((item) => {
       cartQuantity += item.quantity;
@@ -95,3 +106,19 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   });
 });
 
+// (1)
+/*
+  console.log(1)
+  console.log(2)
+  console.log(3)... -> sẽ ra kq: 1, 2, 3... => Đây là đồng bộ (tuần tự)
+
+  setTimeout(() => {
+      console.log(4);
+  },2000)
+  console.log(5);
+  -> sẽ ra kq: 1, 2, 3, 5, 4 => Đây là bất đồng bộ => số 4 sẽ đc vào 1 hàng chờ ưu tiên thấp
+
+  ** Mặc dù chạy kh theo thứ tự nhưng cta có thể kiểm soát được.
+  ** Vì là ưu tiên thấp nên bđb được thực hiện sau, đb được thực hiện trc
+
+*/

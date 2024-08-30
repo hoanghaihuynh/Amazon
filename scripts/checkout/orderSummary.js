804377d7cbe6ac90117a2937fd5b6b22e269f690
HoangHai <honghi.hunh@gmail.com>
2024-08-30 16:00:46 +0700

    Create automatic tests for orderSummary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -29,7 +29,7 @@ export function renderOrderSummary() {
         const dateString = deliveryDate.format('dddd, MMMM DD');
 
         cartSummaryHTML += `
-            <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+            <div class="cart-item-container js-cart-item-container js-cart-item-container-${matchingProduct.id}">
                 <div class="delivery-date">
                     Delivery date: ${dateString}
                 </div>
@@ -45,7 +45,7 @@ export function renderOrderSummary() {
                         <div class="product-price">
                             $${formatCurrency(matchingProduct.priceCents)}
                         </div>
-                        <div class="product-quantity">
+                        <div class="product-quantity js-product-quantity-${matchingProduct.id}">
                             <span>
                                 Quantity: <span class="quantity-label js-quantity-label">${cartItem.quantity}</span>
                             </span>
@@ -56,7 +56,7 @@ export function renderOrderSummary() {
                             <span class="save-quantity-link link-primary js-save-quantity-link" data-product-id="${matchingProduct.id}"> 
                                 Save
                             </span>
-                            <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+                            <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
                                 Delete
                             </span>
                         </div>
@@ -106,8 +106,8 @@ export function renderOrderSummary() {
         link.addEventListener('click', () => {
             const productId = link.dataset.productId;
             removeFromCart(productId);
-			renderCheckoutHeader();
             renderOrderSummary();
+			renderCheckoutHeader();
             renderPaymentSummary();
         })
     });

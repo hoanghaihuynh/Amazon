5c0c8949d669610d1cb823161cc7c77afb738737
HoangHai <honghi.hunh@gmail.com>
2024-09-06 14:28:37 +0700

    Add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummary } from '../scripts/checkout/orderSummary.js';
 import {renderPaymentSummary} from '../scripts/checkout/paymentSummary.js';
 import {renderCheckoutHeader} from './checkout/checkoutHeader.js';
+import '../data/backend-practice.js'
 // import '../data/cart-class.js';
 
 renderCheckoutHeader();
